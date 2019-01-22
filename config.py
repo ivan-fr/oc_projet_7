d56141b820597610a86b97c9f64e0c2163b75559
besevic <ivan.besevic_fr@yahoo.com>
2019-01-22 19:11:00 +0100

    Projet final en mode dev.

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STOP_WORDS_FILE = os.path.join(BASE_DIR, "grandpyapp", "static",
                                "stop_words.json")
 
-GOOGLE_API_KEY = "YOUR API_KEY"
+GOOGLE_API_KEY = "AIzaSyCIB8gP3P5S-ttaOCZQBj0efd8sSDbPqdQ"
